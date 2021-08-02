2482930986691a7214bc928be0443ac6ed07d2e2
Linux Kyrios <malinowskim92@protonmail.com>
2021-08-03 01:10:14 +0200

    Created functions.php file with initial content

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+    $dbhost = 'localhost';
+    $dbname = 'kyriosbook';
+    $dbuser = 'KyriosBook';
+    $dbpassword = 'KyriosBook';
+    $appname = 'KyriosBook';
+    $connection = new mysqli($dbhost, $dbuser, $dbpassword, $dbname);
